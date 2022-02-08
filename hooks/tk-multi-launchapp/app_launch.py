47a31387c4b6de72df1007f825ffeb0751433c17
no-glue <nikolapav1985@gmail.com>
2022-02-08 14:20:00 +0100

    tk multi launchapp bat nk run

--- hooks/tk-multi-launchapp/app_launch.py
+++ hooks/tk-multi-launchapp/app_launch.py
@@ -56,9 +56,11 @@ class AppLaunch(tank.Hook):
         myengine=os.environ["TK_TK_MY"]
         nkengine=os.environ["TK_TK_NK"]
         pathtstbat="%s\\pipeline\\crater-meta\\lib\\cs_studio3\\p3_start_local_process.bat"
+        pathtstbatnk="%s\\pipeline\\crater-meta\\lib\\cs_studio3\\p3_start_local_sg3nk13.bat"
         otherinfo=None
 
         pathtstbat=pathtstbat % environdirlcla
+        pathtstbatnk=pathtstbatnk % environdirlcla
         otherinfo=[version,environtest,type(environtest),app_path,app_args,engine_name,pathtstbat]
 
         loggerinfo.extend(otherinfo)
@@ -102,7 +104,7 @@ class AppLaunch(tank.Hook):
             # on windows, we run the start command in order to avoid
             # any command shells popping up as part of the application launch.
             if environtest == "TRUE":
-                cmd = 'start /B "App" "%s" %s' % (pathtstbat, "\"%s\" %s" % (app_path, app_args))
+                cmd = 'start /B "App" "%s" %s' % (pathtstbatnk, "\"%s\" %s" % (app_path, app_args))
             else:
                 cmd = 'start /B "App" "%s" %s' % (app_path, app_args)
 
